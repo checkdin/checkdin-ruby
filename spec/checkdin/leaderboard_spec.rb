c71b3016a0da1107aae6e10e4aee387ec385441e
Laust Rud Jacobsen <laust@object.io>
2013-10-14 16:15:09 +0200

    Leaderboard: upgrade vcr

--- spec/checkdin/leaderboard_spec.rb
+++ spec/checkdin/leaderboard_spec.rb
@@ -6,35 +6,33 @@ describe Checkdin::Leaderboard do
     @client = Checkdin::Client.new(TestCredentials.client_args)
   end
 
-  context "viewing a leaderboard for the client" do
-    use_vcr_cassette
+  context "viewing a leaderboard for the client", :vcr do
     let(:result) { @client.leaderboard(:limit => 5) }
 
     it "should provide a list of point accounts" do
-      result.point_accounts.first.point_account.user.username.should == "leader"
-      result.point_accounts.first.point_account.balance.should == 961
-      result.point_accounts.count.should == 5
+      result.point_accounts.first.point_account.user.username.should == "primero"
+      result.point_accounts.first.point_account.balance.should == 50
+      result.point_accounts.count.should == 1
     end
   end
 
-  context "viewing a leaderboard for a specific campaign" do
-    use_vcr_cassette
-    let(:result) { @client.campaign_leaderboard(8) }
+  context "viewing a leaderboard for a specific campaign", :vcr do
+    let(:result) { @client.campaign_leaderboard(1) }
 
     it "should provide a list of point accounts" do
-      result.point_accounts.first.point_account.user.username.should == "leader"
-      result.point_accounts.first.point_account.balance.should == 53
+      result.point_accounts.first.point_account.user.username.should == "primero"
+      result.point_accounts.first.point_account.balance.should == 71
       result.point_accounts.count.should == 1
     end
-  end  
+  end
 
-  context "viewing a leaderboard for a campaign's classifications" do
-    use_vcr_cassette
-    let(:result) { @client.classification_leaderboard(8) }
+  context "viewing a leaderboard for a campaign's classifications", :vcr do
+    let(:result) { @client.classification_leaderboard(1) }
 
     it "should provide a list of classifications" do
-      result.classifications.first.classification.should == "grouped"
-      result.classifications.first.total_points.should == 53
+      result.classifications.first.classification.should == "mugglers"
+      result.classifications.first.total_points.should == 71
     end
   end
 end
+
